671e857b64b00d3d3117d08c791f41f41fbc7215
Loren Mariquit <l.mariquit@gmail.com>
2019-03-29 22:55:31 -0400

    fixed miliseconds. Timer looks to work well

--- client/components/Timer.js
+++ client/components/Timer.js
@@ -20,13 +20,14 @@ class Timer extends Component {
   }
 
   beginTimer() {
-    this.setState({
-      start: Date.now()
-    })
+    // this.setState({
+    //   start: Date.now()
+    // })
     // let start = Date.now()
     // let start = Date.now() - 7190000
     this.interval = setInterval(() => {
-      let delta = Date.now() - this.state.start
+      // console.log('start', this.state.start, 'idle', this.state.timeIdle)
+      let delta = Date.now() - this.state.start - this.state.timeIdle
       let secsToDisplay = this.state.secs
       let msecsToDisplay = this.state.msecs
       let minsToDisplay = this.state.mins
@@ -39,12 +40,14 @@ class Timer extends Component {
         secsToDisplay = secsSinceStart.toString()
       }
 
-      let msecsSinceStart = Math.floor((delta - secsSinceStart) % 100)
+      // let msecsSinceStart = Math.floor(delta % 100)
+      let msecsSinceStart = Math.floor((delta / 10) % 100)
       if (msecsSinceStart < 10) {
         msecsToDisplay = `0${msecsSinceStart.toString()}`
       } else {
         msecsToDisplay = msecsSinceStart.toString()
       }
+      console.log(msecsToDisplay, msecsSinceStart, secsToDisplay)
 
       let minsSinceStart = Math.floor(delta / 1000 / 60) % 60
       if (minsSinceStart < 60) {
@@ -81,17 +84,22 @@ class Timer extends Component {
       })
       this.pauseTimer()
     } else {
-      if (this.state.pauseTime !== 0) {
+      if (this.state.start === 0) {
+        this.setState({
+          start: Date.now(),
+          toggle: true
+        })
+        console.log('HIT')
+      } else {
         let timeToStart = Date.now()
-        let timeIdle = this.state.pauseTime - timeToStart
+        let timeIdle = timeToStart - this.state.pauseTime
+        // console.log(timeIdle)
         this.setState({
           pauseTime: 0,
-          timeIdle
+          timeIdle: this.state.timeIdle + timeIdle,
+          toggle: true
         })
       }
-      this.setState({
-        toggle: true
-      })
       this.beginTimer()
     }
   }
