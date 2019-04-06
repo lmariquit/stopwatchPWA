a056d3d99c5846b3b69e9598b9a0d306307bcfcd
Loren Mariquit <l.mariquit@gmail.com>
2019-04-05 20:30:39 -0400

    reset feature added. Stopwatch background image also added, styled for mobile. Need to style for desktop.

--- client/components/Timer.js
+++ client/components/Timer.js
@@ -6,6 +6,7 @@ class Timer extends Component {
     this.beginTimer = this.beginTimer.bind(this)
     this.pauseTimer = this.pauseTimer.bind(this)
     this.toggleTimer = this.toggleTimer.bind(this)
+    this.resetTimer = this.resetTimer.bind(this)
     this.interval
     this.state = {
       toggle: false,
@@ -99,10 +100,25 @@ class Timer extends Component {
     }
   }
 
+  resetTimer() {
+    if (!this.state.toggle) {
+      this.setState({
+        start: 0,
+        pauseTime: 0,
+        timeIdle: 0,
+        hrs: '00',
+        mins: '00',
+        secs: '00',
+        msecs: '00'
+      })
+    }
+  }
+
   render() {
     return (
       <div id="stopwatch">
         <div id="timer-container">
+          <div id="timer-image" />
           <div id="timer">
             <div id="hrs">{this.state.hrs}</div>
             <div className="colons">:</div>
@@ -113,6 +129,9 @@ class Timer extends Component {
           </div>
         </div>
         <div id="button-container">
+          <div id="reset-button" onClick={() => this.resetTimer()}>
+            RESET
+          </div>
           <div id="start-button" onClick={() => this.toggleTimer()}>
             {this.state.buttonText}
           </div>
