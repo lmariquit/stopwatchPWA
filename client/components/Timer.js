c6682c0cd5a1cc4026aed271c0f574bdd1683a9c
Loren Mariquit <l.mariquit@gmail.com>
2019-04-08 13:25:59 -0400

    removed nosleep js. It didnt work

--- client/components/Timer.js
+++ client/components/Timer.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react'
-import NoSleep from 'nosleep.js'
 
 const Timer = () => {
-  const noSleep = new NoSleep()
   let timeInterval
   let resetOpacity
   const [toggle, setToggle] = useState(false)
@@ -23,22 +21,15 @@ const Timer = () => {
     function beginOrPauseTimer() {
       if (toggle) {
         beginTimer()
-        document.addEventListener('touchstart', enableNoSleep, false)
       } else {
         pauseTimer()
       }
       return () => {
         clearInterval(timeInterval)
-        noSleep.disable()
       }
     },
     [toggle]
   )
-
-  function enableNoSleep() {
-    noSleep.enable()
-    document.removeEventListener('touchstart', enableNoSleep, false)
-  }
 
   function beginTimer() {
     timeInterval = setInterval(() => {
